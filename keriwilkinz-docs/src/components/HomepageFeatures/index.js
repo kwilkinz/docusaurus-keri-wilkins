392859c701e76cf33ede983ee73027e2c7667be6
Keri Wilkins <keriannwilkins@gmail.com>
2024-01-10 09:35:25 -0600

    changes to the home page (features)

--- keriwilkinz-docs/src/components/HomepageFeatures/index.js
+++ keriwilkinz-docs/src/components/HomepageFeatures/index.js
@@ -1,43 +1,47 @@
-import clsx from 'clsx';
-import Heading from '@theme/Heading';
-import styles from './styles.module.css';
+import clsx from "clsx";
+import Heading from "@theme/Heading";
+import styles from "./styles.module.css";
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: "Featured Works",
+    Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Explore my project collection that is dedicated to creativity,
+        innovation, and excellence. From impactful friendly UI designs to
+        solutions, each project showcases my expertise and commitment to
+        delivering exceptional results.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: "My Story",
+    Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Get to know me better! Discover my story behind my passion-driven
+        journey.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: "Insight Hub",
+    Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Dive into my repository of articles, thought-provoking insights, and
+        practical tips. This blog serves as a knowledge center where we share
+        expertise, trends, and experiences, empowering you to stay informed and
+        inspired in your endevours.
       </>
     ),
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, description }) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
